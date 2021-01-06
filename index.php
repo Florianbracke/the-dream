fb71ef347ad7853db3d8af762db5d6368a07b7a5
Florianbracke <florianbracke@gmail.com>
2021-01-06 11:05:10 +0100

    add conversion

--- index.php
+++ index.php
@@ -10,27 +10,67 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+//currency rates
+
 ?>
 
 <h1>Welcome!</h1>
-<!-- local rate to euro -->
-<form action="" method="post">
-Local rate: <input type="text" name="localToEuros"><br>
-<input type="submit">
 
+<form  action="" method="post">
 
-<br><br>
-your local rate will get you:
+    <div id="box"> 
+        <h2>Currency Converter</h2>
+    <table>
+        <tr>
+            <td>
+                Enter Amount:<input type="text" name="amount"><br>
+            </td>
+        </tr>
+        <tr>
+        <td>
+            <br>From:<select name='fromCurrency'>
+                <option value="EUR" selected>Euro(EUR)</option>
+                <option value="USD" >US Dollar(USD)</option>
+        
+            </select>
+        </td>
+        </tr>
+        <tr>
+            <td>
+            <br>To:<select name='toCurrency'>
+                <option value="RS" selected >Indonesian Rupiah(RS)</option>
+            </select>
+        </td>
+        </tr>
+        <tr>
+        <td><br>
+            <input type='submit' name='submit' value="Convert">
+        </td>
+        </tr>
+    </table></div>
+</form>
+
 <?php
+// display what currency is being conversed
+// $money = 4;
 
-echo $_POST["localToEuros"]; ?> 
-euros
+// function Conversion($money){
+//     echo "your $money is worth ";
+// }
+
+// if(isset($_POST['toCurrency'])){
+//     function Conversion($money);
+// }
+
+// does the correct conversion
+if(isset($_POST['amount'])){
+    
+    $amount = $_POST["amount"];
+    echo $amount * 17167,24;
+}
+?> RS
 
 
 </body>
 </html>
-
-<?php 
-
-
-?>
